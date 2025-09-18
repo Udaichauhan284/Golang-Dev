960032617e588445041bc3bede1133c9d3b64e8a
Udai Chauhan <udaichauhan284@gmail.com>
2025-09-19 02:00:42 +0530

    ADD deleteURL routes

--- url-shortener/api/routes/deleteUrl.go
+++ url-shortener/api/routes/deleteUrl.go
@@ -1,1 +1,27 @@
 package routes
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/udaichauhan/url_shortener/api/database"
+)
+
+func DeleteURL(c *gin.Context){
+	shortID := c.Param("shortID");
+
+	r := database.CreateClient(0);
+	defer r.Close();
+
+	err := r.Del(database.Ctx, shortID).Err();
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error" : "Unable to Delete shortened Link",
+		});
+		return;
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message" : "Shortend URL Deleted Successfully",
+	});
+}
--- url-shortener/main.go
+++ url-shortener/main.go
@@ -31,4 +31,6 @@ func main(){
 func setUpRouters(router *gin.Engine){
 	router.POST("/api/v1", routes.ShortenURL);
 	router.GET("/api/v1/:shortID", routes.GetByShortID);
+	router.DELETE("/api/v1/:shortID", routes.DeleteURL);
+	router.PUT("/api/v1/:shortID", routes.EditURL);
 }
